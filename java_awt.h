1ac3da38a1388fddc6bb388befa4ba8dc6ec3d73
John Holdsworth <github@johnholdsworth.com>
2016-12-20 13:04:42 +0000

    Initial commit

---
+++ java_awt.h
@@ -0,0 +1,19 @@
+//
+//  java_awt.h
+//  java_awt
+//
+//  Created by John Holdsworth on 13/07/2016.
+//  Copyright (c) 2016 John Holdsworth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for java_awt.
+FOUNDATION_EXPORT double java_awtVersionNumber;
+
+//! Project version string for java_awt.
+FOUNDATION_EXPORT const unsigned char java_awtVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <java_awt/PublicHeader.h>
+
+
